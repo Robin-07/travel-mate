9e6db41c27566edb2215a5e3bdc71ed583da1565
Robin <robinrythm123@gmail.com>
2021-09-27 18:56:28 +0530

    minor change

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,6 +1,1 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.demo, name="demo")
-]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-
-def demo(request):
-    return HttpResponse("<h1>Welcome</h1>")
