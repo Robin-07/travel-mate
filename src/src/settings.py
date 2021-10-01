9fedeca2170eb6299ee8422d60d1ccc830a8f010
Robin <robinrythm123@gmail.com>
2021-10-01 15:11:49 +0530

    some UI improvements

--- src/src/settings.py
+++ src/src/settings.py
@@ -151,6 +151,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'authentication/static/'),
+    os.path.join(BASE_DIR, 'home/static/'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
